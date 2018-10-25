6db35ffd18840d7937b2a7ee8c18049748623cfa
John Asare <jasar002@plattsburgh.edu>
2018-10-25 14:37:52 -0400

    Created two files. The test file and the function file

---
+++ lists.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def sumOfOdd(intlist):
+    pass
+
+def productOfEven(intlist):
+    pass
+
+def changeList(intlist):
+    pass
+
+def membersDivisibleBy3(intlist):
+    pass
+
+def isReverse(intlist_one, intlist_two):
+    pass
